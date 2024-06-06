5451df20d061030589278fe008bc4533834874c1
Saim Shahzad <saimshehzad2030@gmail.com>
2024-06-06 19:49:33 +0500

    chore: fixed some bug

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,13 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 export default function Home() {
-  // const router = useRouter();
-  // useEffect(() => {
-  //   router.push("https://www.inappsolutions.com/");
-  // },[])
+  const router = useRouter();
+  useEffect(() => {
+    router.push("https://www.google.com/");
+  },[])
   return (
     <>
-    {/* Redirecting... */}
+    Redirecting...
     <RedirectToBrowser/>
     </>
     
--- src/components/Redirect-to-browser/Redirect.component.jsx
+++ src/components/Redirect-to-browser/Redirect.component.jsx
@@ -1,4 +1,4 @@
- "use client"
+"use client"
 import { useEffect,useState } from 'react';
 import { useRouter } from 'next/navigation';
 
@@ -7,21 +7,28 @@ const RedirectToBrowser = () => {
     const router = useRouter();
 
   useEffect(() => {
-    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    const userAgent = navigator.userAgent  || window.opera;
 
     const isAndroid = /Android/i.test(userAgent);
     const isiOS = /iPhone|iPad|iPod/i.test(userAgent);
 
-    const targetUrl = "https://www.inappsolutions.com/" + router.asPath;
+    const targetUrl = "https://www.google.com/" + router.asPath;
 
     if (isAndroid) {
       window.location.href = `intent://${targetUrl.replace('https://', '')}#Intent;scheme=https;package=com.android.chrome;end`;
-    } else if (isiOS) {
-      window.location.href = targetUrl;
+    } 
+    else if (isiOS) {
+        const isSafari = navigator.userAgent.includes('Safari') && !navigator.userAgent.includes('Chrome')
+         
+        if (isSafari) {
+            window.location.href = targetUrl;
+        } else {
+            window.location.href = `https://www.google.com/apple-app-site-association`;
+        }
     }
     else{
         setLoading(false);
-        router.push("https://www.inappsolutions.com/");
+        router.push("https://www.google.com/");
     }
   }, [router]);
 
